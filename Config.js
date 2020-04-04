6c72ab2d625771f37ee13d175980d56bbffc5b9e
Raigedas Radišauskas <raigedas@dmprojects.com>
2020-04-04 12:54:34 +0300

    do not send null values to save network traffic

--- Config.js
+++ Config.js
@@ -15,6 +15,17 @@ config.interceptors.preSelect = [];
 config.interceptors.postSelect = [];
 
 config.interceptors.postSelect.push((req, tableName, rows) => {
+    rows.forEach(e => {
+        const propertyNames = Object.getOwnPropertyNames(e);
+        propertyNames.forEach(propertyName => {
+            if (e[propertyName] === null) {
+                e[propertyName] = undefined;
+            }
+        });
+    });
+    return rows;
+});
+config.interceptors.postSelect.push((req, tableName, rows) => {
     return util.convertObjectsStyle(rows, config.propertyNameConverter.toJs);
 })
 
